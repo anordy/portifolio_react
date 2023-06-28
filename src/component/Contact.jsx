2ed2d9d04abdda8251124526ed71fca93a6a4360
anordy <anordypatrick98@gmail.com>
2023-06-28 16:17:42 +0300

    send contact form

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -7,16 +7,16 @@ export default class Contact extends Component {
           <section class="contact" id="contact">
         <h2 class="heading">Contact <span>Me!</span></h2>
 
-        <form action="#">
+        <form action="https://formspree.io/f/mgejwlvq" method="POST">
             <div class="input-box">
-                <input type="text" placeholder="Full Name" />
-                <input type="text" placeholder="Email Address" />
+                <input type="text" name="name" placeholder="Full Name" />
+                <input type="text" name="email" placeholder="Email Address" />
             </div>
             <div class="input-box">
-                <input type="number" placeholder="Mobile Number" />
-                <input type="text" placeholder="Email Subject" />
+                <input type="number" name="mobile" placeholder="Mobile Number" />
+                <input type="text" name="subject" placeholder="Email Subject" />
             </div>
-            <textarea name="" id="" placeholder="Your Message" cols="30" rows="10"></textarea>
+            <textarea name="body" id="" placeholder="Your Message" cols="30" rows="10"></textarea>
             <input type="submit" value="Send Message" class="btn" />
         </form>
     </section>
